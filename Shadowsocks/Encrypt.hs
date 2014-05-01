f58fc39c3fea4e0b9be3b0a12636a52dcc716648
rnons <remotenonsense@gmail.com>
2014-05-01 19:46:18 +0800

    Initial openssl support.

--- Shadowsocks/Encrypt.hs
+++ Shadowsocks/Encrypt.hs
@@ -1,12 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Shadowsocks.Encrypt where
 
+import Control.Concurrent.MVar (MVar, newEmptyMVar, isEmptyMVar, putMVar, readMVar)
 import Crypto.Hash.MD5 (hash)
 import Data.Binary.Get (runGet, getWord64le)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy as L
+import Data.IntMap.Strict (fromList, (!))
+import Data.Maybe (fromJust)
 import Data.Monoid ((<>))
 import Data.Word (Word8, Word64)
+import OpenSSL (withOpenSSL)
+import OpenSSL.EVP.Cipher
+import OpenSSL.Random (randBytes)
+import System.IO.Unsafe (unsafePerformIO)
+import Data.Char
 
 getTable :: ByteString -> [Word8]
 getTable key = do
@@ -30,7 +40,7 @@ cmpsort (x:xs) cmp =
      in cmpsort left cmp ++ [x] ++ cmpsort right cmp
 
 evpBytesToKey :: ByteString -> Int -> Int -> (ByteString, ByteString)
-evpBytesToKey password keyLen ivLen = do
+evpBytesToKey password keyLen ivLen =
     let ms' = S.concat $ ms 0 []
         key = S.take keyLen ms'
         iv  = S.take ivLen $ S.drop keyLen ms'
@@ -42,3 +52,67 @@ evpBytesToKey password keyLen ivLen = do
         | S.length (S.concat m) < keyLen + ivLen =
             ms (i+1) (m ++ [hash (last m <> password)])
         | otherwise = m
+
+getCipher :: ByteString -> CryptoMode -> IO (ByteString -> IO ByteString)
+getCipher iv mode = do
+    let (key, iv_) = evpBytesToKey "abc" 16 8
+    method <- fmap fromJust $ withOpenSSL $ getCipherByName "bf-cfb"
+    return $ cipherBS method (C.unpack key) (C.unpack iv) mode
+
+cipherMVar :: MVar ()
+cipherMVar = unsafePerformIO newEmptyMVar
+decipherMVar :: MVar (ByteString -> IO ByteString)
+decipherMVar = unsafePerformIO newEmptyMVar
+
+getEncDec :: ByteString 
+          -> IO (ByteString -> IO ByteString, ByteString -> IO ByteString)
+getEncDec iv = do
+
+    random_iv <- if S.null iv then withOpenSSL $ randBytes 32 
+                              else return iv
+    let cipher_iv = S.take 8 random_iv
+
+    myCipher <- getCipher random_iv Encrypt
+    let 
+        encrypt "" = return ""
+        encrypt buf = do
+            empty <- isEmptyMVar cipherMVar
+            if empty 
+                then do
+                    putMVar cipherMVar ()
+                    ciphered <- withOpenSSL $ myCipher buf
+                    print cipher_iv 
+                    print ciphered 
+                    return $ cipher_iv <> ciphered
+                else
+                    withOpenSSL (myCipher buf)
+        decrypt "" = return ""
+        decrypt buf = do
+            empty <- isEmptyMVar decipherMVar
+            if empty 
+                then do
+                    print $ S.length buf
+                    let decipher_iv = S.take 8 buf
+                    myDecipher <- getCipher decipher_iv Decrypt
+                    putMVar decipherMVar myDecipher
+                    if S.null (S.drop 8 buf)
+                        then return ""
+                        else withOpenSSL $ myDecipher (S.drop 8 buf)
+                else do
+                    myDecipher <- readMVar decipherMVar
+                    withOpenSSL $ myDecipher buf
+
+    return (encrypt, decrypt)
+
+getTableEncDec :: ByteString
+          -> IO (ByteString -> IO ByteString, ByteString -> IO ByteString)
+getTableEncDec _ = do
+    let table = getTable "abc"
+        encryptTable = fromList $ zip [0..255] table
+        decryptTable = fromList $ zip (map fromIntegral table) [0..255]
+        encrypt :: ByteString -> IO ByteString
+        encrypt buf = return $ S.pack $ map (\b -> encryptTable ! fromIntegral b) $ S.unpack buf
+        decrypt :: ByteString -> IO ByteString
+        decrypt buf = return $ S.pack $ map (\b -> decryptTable ! fromIntegral b) $ S.unpack buf
+
+    return (encrypt, decrypt)
