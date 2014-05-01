6543ff6dce79577c14415d52f19e276631f15f58
rnons <remotenonsense@gmail.com>
2014-05-01 19:47:14 +0800

    Add server.hs

--- server.hs
+++ server.hs
@@ -5,7 +5,6 @@ import Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar, putMVar)
 import Control.Monad (void)
 import Data.Char (ord)
 import Data.Binary.Get (runGet, getWord16be)
-import Data.Binary.Put (runPut, putWord16be)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
@@ -22,12 +21,12 @@ main :: IO ()
 main = withSocketsDo $ do
     addrinfos <- getAddrInfo
                  (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
-                 Nothing (Just "7777")
+                 Nothing (Just "8888")
     let serveraddr = head addrinfos
     sock <- socket (addrFamily serveraddr) Stream defaultProtocol
     bindSocket sock (addrAddress serveraddr)
     listen sock 1
-    putStrLn "starting local at 7777"
+    putStrLn "starting server at 8888"
     (encrypt, decrypt) <- getTableEncDec ""
     sockHandler sock encrypt decrypt
     close sock
@@ -38,33 +37,27 @@ sockHandler :: Socket
             -> IO ()
 sockHandler sock encrypt decrypt = do
     (conn, _) <- accept sock
-    recv conn 262
-    send conn "\x05\x00"
-    msg <- recv conn 4
-    let m = C.unpack msg
+    addrType <- recv conn 1 >>= decrypt
 
-    (addr, addr_to_send') <- if ord (m !! 3) == 1
+    addr <- if ord (head $ C.unpack addrType) == 1
         then do
-            addr_ip <- recv conn 4
-            addr <- inet_addr (C.unpack addr_ip) >>= inet_ntoa
-            return (C.pack addr, addr_ip)
+            addr_ip <- recv conn 4 >>= decrypt
+            inet_addr (C.unpack addr_ip) >>= inet_ntoa
         else do
-            addr_len <- recv conn 1
-            addr <- recv conn (ord $ head $ C.unpack addr_len)
-            return (addr, addr_len <> addr)
+            addr_len <- recv conn 1 >>= decrypt
+            addr <- recv conn (ord $ head $ C.unpack addr_len) >>= decrypt
+            return $ C.unpack addr
 
-    addr_port <- recv conn 2
-    let addr_to_send = S.singleton (msg `S.index` 3) <> addr_to_send' <> addr_port
-        port = runGet getWord16be $ L.fromStrict addr_port
-    let reply = "\x05\x00\x00\x01" <> "\x00\x00\x00\x00" <>
-                L.toStrict (runPut $ putWord16be 2222)
-    sendAll conn reply
+    addr_port <- recv conn 2 >>= decrypt
+    let port = runGet getWord16be $ L.fromStrict addr_port
 
-    serverAddr <- getServer
-    remote <- socket (addrFamily serverAddr) Stream defaultProtocol
-    connect remote (addrAddress serverAddr)
-    encrypt addr_to_send >>= sendAll remote
-    C.putStrLn $ "connecting " <> addr <> ":" <> C.pack (show port)
+    remoteAddr <- fmap head $ 
+        getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
+                    (Just addr)
+                    (Just $ show port)
+    remote <- socket (addrFamily remoteAddr) Stream defaultProtocol
+    connect remote (addrAddress remoteAddr)
+    putStrLn $ "connecting " <> addr <> ":" <> show port
     let localwait = unsafePerformIO newEmptyMVar
         remotewait = unsafePerformIO newEmptyMVar
     forkIO $ handleTCP conn remote encrypt decrypt localwait remotewait
@@ -91,12 +84,12 @@ handleTCP conn remote encrypt decrypt localwait remotewait = do
     close remote
   where
     handleLocal = do
-        inData <- recv conn 4096 >>= encrypt
+        inData <- recv conn 4096 >>= decrypt
         if S.null inData
             then putMVar localwait ()
             else sendAll remote inData >> handleLocal
     handleRemote = do
-        inData <- recv remote 4096 >>= decrypt
+        inData <- recv remote 4096 >>= encrypt
         if S.null inData
             then putMVar localwait ()
             else sendAll conn inData >> handleRemote
