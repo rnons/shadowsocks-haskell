f28b8e21cf75b763a155b78ced1a49f73a296301
rnons <remotenonsense@gmail.com>
2014-10-07 10:28:34 +0800

    Update to optparse-applicative-0.11

--- Shadowsocks/Util.hs
+++ Shadowsocks/Util.hs
@@ -10,7 +10,6 @@ import qualified Data.ByteString.Lazy as L
 import           Data.Maybe (fromMaybe)
 import           GHC.Generics (Generic)
 import           Options.Applicative
-import           Options.Applicative.Types (ReadM(ReadM))
 
 data Config = Config
     { server        :: String
@@ -38,32 +37,20 @@ nullConfig = Config "" 0 0 "" 0 ""
 readConfig :: FilePath -> IO (Maybe Config)
 readConfig fp = liftM decode' $ L.readFile fp
 
-optStr :: Mod OptionFields (Maybe String) -> Parser (Maybe String)
-optStr m =
-    nullOption $ value Nothing <> reader (success . str) <> m
-  where
-    success = ReadM . Right
-
-maybeOpt :: Read a => Mod OptionFields (Maybe a) -> Parser (Maybe a)
-maybeOpt m =
-    nullOption $ value Nothing <> reader (success . auto) <> m
-  where
-    success = ReadM . Right
-
 configOptions :: Parser Options
 configOptions = Options
-    <$> optStr (long "server" <> short 's' <> metavar "ADDR"
-               <> help "server address")
-    <*> maybeOpt (long "server-port" <> short 'p' <> metavar "PORT"
-                 <> help "server port")
-    <*> maybeOpt (long "local-port" <> short 'l' <> metavar "PORT"
-                 <> help "local port")
-    <*> optStr (long "password" <> short 'k' <> metavar "PASSWORD"
-               <> help "password")
-    <*> optStr (long "method" <> short 'm' <> metavar "METHOD"
-               <> help "encryption method, for example, aes-256-cfb")
-    <*> optStr (long "config" <> short 'c' <> metavar "CONFIG"
-               <> help "path to config file")
+    <$> optional (strOption (long "server" <> short 's' <> metavar "ADDR"
+              <> help "server address"))
+    <*> optional (option auto (long "server-port" <> short 'p' <> metavar "PORT"
+                <> help "server port"))
+    <*> optional (option auto (long "local-port" <> short 'l' <> metavar "PORT"
+                <> help "local port"))
+    <*> optional (strOption (long "password" <> short 'k' <> metavar "PASSWORD"
+              <> help "password"))
+    <*> optional (strOption (long "method" <> short 'm' <> metavar "METHOD"
+              <> help "encryption method, for example, aes-256-cfb"))
+    <*> optional (strOption (long "config" <> short 'c' <> metavar "CONFIG"
+               <> help "path to config file"))
 
 parseConfigOptions :: IO Config
 parseConfigOptions = do
