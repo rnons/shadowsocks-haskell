3e3d56aa1c6fdfbe92bb885e6661eaf9f84ed082
rnons <remotenonsense@gmail.com>
2015-09-21 15:32:25 +0800

    use unpackRequest in server.hs

--- server.hs
+++ server.hs
@@ -2,18 +2,12 @@
 
 import           Conduit (Sink, await, liftIO, (=$), ($$), ($$+), ($$+-))
 import           Control.Concurrent.Async (race_)
-import           Data.Binary (decode)
-import           Data.Binary.Get (runGet, getWord16be, getWord32le)
 import           Data.ByteString (ByteString)
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
-import           Data.Char (ord)
 import           Data.Conduit.Network ( runTCPServer, runTCPClient
                                       , serverSettings, clientSettings
                                       , appSource, appSink)
 import           Data.Monoid ((<>))
-import           Data.IP (fromHostAddress, fromHostAddress6)
 import           GHC.IO.Handle (hSetBuffering, BufferMode(NoBuffering))
 import           GHC.IO.Handle.FD (stdout)
 
@@ -22,29 +16,11 @@ import Shadowsocks.Util
 
 initRemote :: (ByteString -> IO ByteString)
            -> Sink ByteString IO (ByteString, Int)
-initRemote decrypt = await >>= 
+initRemote decrypt = await >>=
     maybe (error "Invalid request") (\encRequest -> do
         request <- liftIO $ decrypt encRequest
-        let addrType = S.head request 
-            request' = S.drop 1 request
-        (addr, addrPort) <- case addrType of
-            1 -> do     -- IPv4
-                let (ip, rest) = S.splitAt 4 request'
-                    addr = C.pack $ show $ fromHostAddress $ runGet getWord32le
-                                                           $ L.fromStrict ip
-                return (addr, S.take 2 rest)
-            3 -> do     -- domain name
-                let addrLen = ord $ C.head request'
-                    (domain, rest) = S.splitAt (addrLen + 1) request'
-                return (S.tail domain, S.take 2 rest)
-            4 -> do     -- IPv6
-                let (ip, rest) = S.splitAt 16 request'
-                    addr = C.pack $ show $ fromHostAddress6 $ decode
-                                                            $ L.fromStrict ip
-                return (addr, S.take 2 rest)
-            _ -> error $ C.unpack $ S.snoc "Unknown address type: " addrType 
-        let port = fromIntegral $ runGet getWord16be $ L.fromStrict addrPort
-        return (addr, port))
+        let (_, destAddr, destPort, _) = unpackRequest request
+        return (destAddr, destPort))
 
 main :: IO ()
 main = do
